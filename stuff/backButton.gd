955b7b9b8f609d4df6976aa1f1146748de31e2d9
Эроса Челдан <denmencan@gmail.com>
2025-11-18 18:23:01 +0600

    added popup to exiting to menu from sorter

--- stuff/backButton.gd
+++ stuff/backButton.gd
@@ -1,4 +1,11 @@
 extends Button
 
+@onready var popup = $"../ExitPopup"
+
 func _ready() -> void:
-	button_down.connect(SceneManager.loadMenu)
+	button_down.connect(openPopup.bind(true))
+	$"../ExitPopup/Panel/yes".button_down.connect(SceneManager.loadMenu)
+	$"../ExitPopup/Panel/no".button_down.connect(openPopup.bind(false))
+
+func openPopup(value = true):
+	popup.visible = value
